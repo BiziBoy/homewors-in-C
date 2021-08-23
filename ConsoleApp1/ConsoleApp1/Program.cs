b97ee315dfbf4a0162e871fd9a3d9ebcec3ac01e
BiziBoy <79360682+BiziBoy@users.noreply.github.com>
2021-08-23 17:24:08 +0300

    Подключил и проверил свою библиотеку для работы с массивами.

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using MyArrayLibrary;
+
+namespace ConsoleApp1
+{
+  class Program 
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello, world!");
+      int[] array = new int[5] { 1, 3, 2, 4, 5 };
+      MyArray.addToStart(ref array);
+      foreach (var item in array)
+      {
+        Console.WriteLine(item);
+      }
+      Console.ReadLine();
+
+    }
+  }
+}
